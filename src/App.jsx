f024c957b309acbb58835c3e05e878fec7c1f231
Pratik Parida <paridapratik709@gmail.com>
2025-03-01 17:03:14 +0530

    Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import { Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Collection from './pages/Collection';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Order from './pages/Order';
+import PlaceOrder from './pages/PlaceOrder';
+import Product from './pages/Product';
+import NavBar from './components/NavBar';
 const App = () => {
   return (
-    <div className='text-slate-600'>
-      E Commerce Project
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/order' element={<Order/>}/>
+      </Routes>
     </div>
   )
 }
