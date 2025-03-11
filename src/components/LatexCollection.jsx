1f36eae7020658d222d681ac9a964a2ff9dd6e1e
Pratik Parida <paridapratik709@gmail.com>
2025-03-11 13:30:50 +0530

    Best Seller Section

--- src/components/LatexCollection.jsx
+++ src/components/LatexCollection.jsx
@@ -10,12 +10,12 @@ const LatexCollection = () => {
       <div className='text-center py-8 text-3xl'>
             <Title text1={'Latest'} text2={'Arrival'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, quas saepe? Accusamus!
+            Fresh new arrival – grab yours before it's gone!
             </p>
       </div>
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5'>
 
-        {products.products.map((item, index) => index<10 ?<ShowProduct key={index} id={item.id} name={item.name} image={item.image} price={item.price} />:null)}
+        {products.products.map((item, index) => index<10 ?<ShowProduct key={index} id={item._id} name={item.name} image={item.image} price={item.price} />:null)}
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatexCollection from '../components/LatexCollection'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div >
       <Hero/>
       <LatexCollection/>
+      <BestSeller/>
     </div>
   )
 }
