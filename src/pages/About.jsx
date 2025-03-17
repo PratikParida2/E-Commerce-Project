33bba342bf03a67d9d8e89372f488372c20b674c
Pratik Parida <paridapratik709@gmail.com>
2025-03-17 17:47:41 +0530

    Contact And About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, iusto beatae! Enim voluptatem repellat quisquam illum est error laudantium assumenda, animi explicabo, unde aliquam repellendus nemo! Ea tempore autem possimus illum quo quos. Necessitatibus repellat, unde iure, itaque quo aliquam adipisci laudantium sint ratione eius vero dolores cupiditate nam beatae.</h1>
     </div>
   )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <h1>Contact Page</h1>
+      <h1>gmail:paridapratik709@gmail.com</h1>
+      <h1>phone:</h1>
     </div>
   )
 }
