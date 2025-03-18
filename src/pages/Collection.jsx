2f2982a1abce9170628daaa18ff03e09cf128bb8
Pratik Parida <paridapratik709@gmail.com>
2025-03-18 19:24:38 +0530

    Working On Filtering Product

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -4,7 +4,7 @@ import {useContext,useState} from 'react'
 import ShowProduct from '../components/ShowProduct';
 import { Link } from 'react-router';
 import { assets } from '../assets/frontend_assets/assets';
-
+import Title from '../components/Title';
 const Collection = () => {
   const {products}=useContext(ShopContext);
   const [showFilter,setShowFilter]=useState(false);
@@ -14,8 +14,9 @@ const Collection = () => {
       {/* Filter Section */}
 
         <div className='min-w-60'>
-          <p className='my-2 text-xl flex items-center cursor-pointer gap-2 '>Filters</p>
-          <img className='h-3 sm:hidden ' src={assets.dropdown_icon} alt="" />
+          <p onClick={()=>setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2 '>Filters
+          <img className={`h-3 sm:hidden ${showFilter ?'rotate-90':''}`}  src={assets.dropdown_icon} alt="" />
+          </p>
           {/* Filter by Category */}
           <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter?'':'hidden'} sm:block cursor-pointer`}>
               <p className='mb-1 text-sm font-medium'>Categories</p>
@@ -41,7 +42,7 @@ const Collection = () => {
                   />
                   Women
                 </label>
-         
+          
                 <label className="flex items-center space-x-2 cursor-pointer gap-2">
                   <input 
                     className="w-3" 
@@ -88,9 +89,20 @@ const Collection = () => {
           </div>
         </div>
 
-
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
-        {products.map((item, index) => (<ShowProduct key={index} id={item._id} name={item.name} image={item.image} price={item.price}/>))}
+      {/* Right Side */}
+      <div className='flex-1'> 
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
+            <Title text1={"All"} text2={"Collections"}/>
+            {/* Product Sort */}
+            <select className='border-2 border-gray-400 text-sm px-2'>
+              <option value="Relative">Sort by : Relavent</option>
+              <option value="LowToHigh">Sort by : Low To High</option>
+              <option value="HighToLow" >Sort by : High To Low</option>
+            </select>
+        </div>
+        <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5'>
+                {products.map((item,index)=>{return <ShowProduct key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>})}
+            </div>
       </div>
     </div>
   )
