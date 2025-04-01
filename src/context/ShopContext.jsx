754d84c14402c527f8030018095fe47d868c4b9b
Pratik Parida <paridapratik709@gmail.com>
2025-04-01 20:07:29 +0530

    Frontend Part Is Over Now From Tommrow Work On Backend

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,10 +1,12 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
 export const ShopContext=createContext();
 const ShopContextProvider=(props)=>
 {
 
+    const navigate=useNavigate();
     const currency='₹';
     const deliveryCharge=10;
     
@@ -14,7 +16,14 @@ const ShopContextProvider=(props)=>
         const savedCart = localStorage.getItem("CartItemsData");
         return savedCart ? JSON.parse(savedCart) : {};
       });
-    
+    const upDateCartItem=(itemId,size,quantity)=>
+    {
+        let cartData=structuredClone(cartItem);
+        cartData[itemId][size]=quantity;
+        console.log("quantity in up to date : "+quantity);
+        setCartItem(cartData);
+        localStorage.setItem('CartItemsData',JSON.stringify(cartItem));
+    }    
     const addToCart=async(itemId,size)=>
     {
         if(!size)
@@ -49,13 +58,32 @@ const ShopContextProvider=(props)=>
                 for(const item in cartItem[items])
                 {
                     if(cartItem[items][item]>0)
-                    total=total+1;
+                    total=total+cartItem[items][item];
                 }
             }
+            console.log("total "+total);
+            
             return total;
         }
+    const getCartAmount=()=>
+    {
+        let total=0;
+        for(const items in cartItem)
+        {
+            let productInfo=products.find((product)=>product._id===items);
+            for(const item in cartItem[items])
+            {
+                if(cartItem[items][item]>0)
+                {
+                    total=total+(cartItem[items][item]*productInfo.price);
+                }
+            }
+
+        }
+        return total;
+    }
     const value={
-        products,currency,deliveryCharge,showSearch,setShowSearch,search,setSearch,cartItem,addToCart,getCartCount
+        products,currency,deliveryCharge,showSearch,setShowSearch,search,setSearch,cartItem,addToCart,getCartCount,upDateCartItem,getCartAmount,navigate
     }
     useEffect(()=>{
         localStorage.setItem("CartItemsData",JSON.stringify(cartItem));
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 const Login = () => {
+  const [currentState,setCurrentState]=useState('SignUp');
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">Login</h2>
+        <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">{currentState}</h2>
         <form>
+          {currentState==='SignUp'?        <div className="mb-4">
+            <label className="block text-gray-700">Name</label>
+            <input
+              type="text"
+              className="w-full p-2 border border-gray-300 rounded mt-1 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="Enter your Name"
+              required
+            />
+          </div>:''}
           <div className="mb-4">
             <label className="block text-gray-700">Email</label>
             <input
@@ -26,15 +35,22 @@ const Login = () => {
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition duration-300"
+            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition duration-300 cursor-pointer"
           >
-            Login
+            {currentState}
           </button>
         </form>
-        <p className="text-center text-gray-600 mt-4">
-          Don't have an account? <a href="#" className="text-blue-500 hover:underline">Sign up</a>
-        </p>
+        {currentState==='Login'?        <div className='w-full flex justify-center text-center text-sm mt-[8px]'>
+          <p className='cursor-pointer'>Forget Password</p>
+        </div>:''}
+        {currentState==='SignUp'?<p className="text-center text-gray-600 mt-4">
+          Have an account? <a href="#" onClick={()=>setCurrentState('Login')} className="text-blue-500 hover:underline ">Login</a>
+        </p>:<p className="text-center text-gray-600 mt-4">
+          Dont't Have an account? <a href="#" onClick={()=>setCurrentState('SignUp')} className="text-blue-500 hover:underline ">SignUp</a>
+        </p>}
+    
       </div>
+     
     </div>
   )
 }
